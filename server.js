5eb902547c4faee6200c87ee04571e223c9ae774
yrtemmys <symmetrycality@gmail.com>
2026-02-20 10:23:59 +0000

    /chicken route to get all chickens in a json format

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ con.connect(function(err){
 		console.log(results);
 	})
 
-	con.end();
+//	con.end();
 });
 
 app.use(express.static('./static/'))
@@ -36,7 +36,39 @@ app.get('/',(req, res)=>{
 	res.sendFile('./static/index.html')
 })
 
+app.get('/chicken', (req, res)=>{
+con.connect(function(err){
+	if(err) throw err;
+	console.log("conneciton yay");
+
+	con.query("select * from chicken;",(err, results,fields)=>{
+		if(err) throw err;
+		res.send(results);
+	})
+
+	con.end();
+});
+/*
+
+	let c_name = "margaret";
+	res.send({
+		"name":c_name
+		"chicken_id": 1,
+		"name": null,
+		"feather_color": null,
+		"size": 1,
+		"from_egg": 1,
+		"coop_id": null,
+		"favorite_food_id": null,
+		"affection_meter": null,
+		"sex": null,
+		"alive_state": null
+	}
+  */
+ // },
+})
 
 app.listen(port, ()=>{
 	console.log('Server is listening on port number: $(port)');
 })
+
---
+++ static/index.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("button1")
+button.addEventListener("click", dothething())
+
+function dothething(){
+	console.log("did it !!!!!!!!1111!")
+}
